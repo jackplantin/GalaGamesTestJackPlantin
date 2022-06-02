4a4703fdbc8af4c9191895d1d2985fb15eabd1ea
Jack Plantin <jack.plantin@offerpad.com>
2022-06-02 16:30:02 -0500

    Add project files.

---
+++ GalaGamesTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+
+namespace GalaGamesTestJackPlantin
+{
+    public class GalaGamesTests
+    {
+
+
+        [Test]
+        public void Test1()
+        {
+
+        }
+
+        [Test]
+        public void Test2()
+        {
+
+        }
+
+        [Test]
+        public void Test3()
+        {
+
+        }
+
+        [Test]
+        public void Test4()
+        {
+
+        }
+
+
+
+
+    }
+}
---
+++ WebDriverSetup.cs
@@ -0,0 +1,29 @@
+﻿
+
+using OpenQA.Selenium.Chrome;
+
+namespace GalaGamesTestJackPlantin
+{
+    public class WebDriverSetup
+    {
+        public static OpenQA.Selenium.WebDriver Driver;
+
+        public static void Initialize(string url)
+        {
+
+                    Driver = new ChromeDriver();
+                    Driver.Manage().Window.Maximize();
+                    Driver.Navigate().GoToUrl(url);
+
+        }
+
+        public static void TearDown()
+        {
+            Driver.Quit();
+        }
+
+
+
+
+    }
+}
